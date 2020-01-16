30f17f13d5408ed0a6d5a0588e7b30c2754d7296
fascot <grebennikovsa8@gmail.com>
2020-01-16 23:29:07 +0300

    Module cheker

--- modulecheker.py
+++ modulecheker.py
@@ -1,7 +1,8 @@
 import mydef
 
-print(mydef.CALCULATOR_OBJECT.my_sum(10, 20))
-print(mydef.CALCULATOR_OBJECT.my_composition(1234, 4321)
-print(mydef.GAMES_OBJECT.my_game1())
-print(mydef.GAMES_OBJECT.my_game2())
-print(mydef.CALCULATOR_OBJECT.my_square_root(625))
+ if __name__ == "__main__":
+    print(mydef.CALCULATOR_OBJECT.my_sum(10, 20))
+    print(mydef.CALCULATOR_OBJECT.my_composition(1234, 4321)
+    print(mydef.GAMES_OBJECT.my_game1())
+    print(mydef.GAMES_OBJECT.my_game2())
+    print(mydef.CALCULATOR_OBJECT.my_square_root(625))
