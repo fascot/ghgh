4fb0c864b4a9ad38d3bfd0f1cb23d522ac4a8ac0
fascot <grebennikovsa8@gmail.com>
2020-01-16 23:22:00 +0300

    Module chekergit add modulecheker.py!

---
+++ modulecheker.py
@@ -0,0 +1,7 @@
+import mydef
+
+print(mydef.CALCULATOR_OBJECT.my_sum(10, 20))
+print(mydef.CALCULATOR_OBJECT.my_composition(1234, 4321)
+print(mydef.GAMES_OBJECT.my_game1())
+print(mydef.GAMES_OBJECT.my_game2())
+print(mydef.CALCULATOR_OBJECT.my_square_root(625))
